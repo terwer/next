37a344cd7d3bfbb7a4e4b6fc455acdf4190f4d76
Terwer <csyouwei@gmail.com>
2019-02-12 02:00:44 +0800

    Init j2v8 nuxt versin

--- src/main/webapp/.eslintrc.js
+++ src/main/webapp/.eslintrc.js
@@ -1,22 +1,19 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true
   },
-  extends: ["eslint:recommended", "plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": "off",
-    "no-debugger": "off",
-    "no-unused-vars": [
-      2,
-      {
-        vars: "local",
-        args: "none"
-      }
-    ],
-    semi: ["error", "always"]
-  },
   parserOptions: {
-    parser: "babel-eslint"
-  }
-};
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:prettier/recommended'
+  ],
+  plugins: [
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {}
+}
